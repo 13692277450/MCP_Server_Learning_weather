aeaaff0db640f7bad8ddbcedc25d126f3d913641
13692277450@139.com <13692277450@139.com>
2026-07-08 07:53:57 +0800

    update

---
+++ streamitMain.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Super AI",
+    page_icon="🦋",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        
+    }
+)
+#大标题
+st.title("Super AI")
+st.logo("logo.jpg")
+
