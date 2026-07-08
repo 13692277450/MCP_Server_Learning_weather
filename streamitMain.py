34f9eaa8c7f03dfb498947faca7d844368f3774c
13692277450@139.com <13692277450@139.com>
2026-07-08 17:33:05 +0800

    Update streamitMain.py

--- streamitMain.py
+++ streamitMain.py
@@ -30,12 +30,50 @@ completion = client.chat.completions.create(
 
             )
 #大标题
+system_prompt = '''
+
+你叫%s,现在是用户的真实伴侣,请完全代入伴侣角色。:
+
+规则:
+
+1. 每次必须回2条消息
+
+2. 禁止任何场景或状态描述性文字
+
+3. 匹配用户的语言
+
+4. 回复简短,像微信聊天一样
+
+5. 有需要的话可以用🍁😊等emoji表情
+
+6. 用符合伴侣性格的方式对话
+
+7. 回复的内容,要充分体现伴侣的性格特征
+伴侣性格:
+
+- %s
+
+你必须严格遵守上述规则来回复用户。
+'''
 st.title("Super AI")
 st.logo("logo.jpg")
-prompt = st.chat_input("请输入您的问题")
-system_prompt = "您是一个专业的AI助手，你的名字叫小智"
+# system_prompt = "您是一个专业的AI助手，你的名字叫小智"
 if "messages" not in st.session_state:
     st.session_state.messages = []
+if "nature" not in st.session_state:
+    st.session_state.nature = "川渝婆娘"
+if "nick_name" not in st.session_state:
+    st.session_state.nick_name = "小甜甜"
+prompt = st.chat_input("请输入您的问题:")
+with st.sidebar:  # with st.sidebar 是streamlit的上下文管理器,用于在侧边栏中显示内容
+    st.subheader("系统菜单")
+    nick_name=st.sidebar.text_input("昵称:", placeholder="请输入昵称", value = st.session_state.nick_name )
+    if nick_name:
+        st.session_state.nick_name = nick_name
+    nature = st.text_area("伴侣性格:", placeholder="请输入您的伴侣性格", value = st.session_state.nature )
+    if nature:
+        st.session_state.nature = nature
+
 #展示聊天信息
 for message in st.session_state.messages:
     #简化
@@ -51,11 +89,23 @@ if prompt:
     response = client.chat.completions.create(
         model = "deepseek-chat",
         messages=[
-            {"role": "system", "content": system_prompt},
+            {"role": "system", "content": system_prompt % (st.session_state.nick_name, st.session_state.nature)},  # %s 动态插入昵称和性格
             {"role": "user", "content": prompt},
             *st.session_state.messages],
-        stream=False,
+        stream=True,
     )
-    print("----------- LLM response message:", response.choices[0].message.content) # type: ignore
-    st.chat_message("assistant").write(response.choices[0].message.content) # type: ignore
-    st.session_state.messages.append({"role": "assistant", "content": response.choices[0].message.content})
+    #非流式输出
+    # print("----------- LLM response message:", response.choices[0].message.content) # type: ignore
+    # st.chat_message("assistant").write(response.choices[0].message.content) # type: ignore
+    # st.session_state.messages.append({"role": "assistant", "content": response.choices[0].message.content})
+    #流式输出
+    full_content = st.empty()
+    full_response = ""
+    for chunk in response:
+        if chunk.choices[0].delta.content is not None:
+            full_response += chunk.choices[0].delta.content
+        full_content.chat_message("assistant").write(full_response)
+            
+            
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    
